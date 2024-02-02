6a51a7e61fcd95f34eb4699ef914e91bf9397f95
Kenta Kudo <yd37574@gmail.com>
2024-02-02 16:34:45 +0900

    duration

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"strings"
 	"syscall/js"
+	"time"
 
 	"github.com/KentaKudo/txt2srt"
 )
 
 func main() {
-	c := make(chan struct{}, 0)
+	c := make(chan struct{})
 
 	js.Global().Set("txt2srt", js.FuncOf(run))
 
@@ -20,8 +21,14 @@ func run(_ js.Value, args []js.Value) any {
 	in := args[0].String()
 	r := strings.NewReader(in)
 
+	var durationOption txt2srt.ConverterOption
+	if len(args) == 2 {
+		duration := args[1].Int()
+		durationOption = txt2srt.WithSecondsToAdd(time.Duration(duration) * time.Second)
+	}
+
 	var buf bytes.Buffer
-	txt2srt.Convert(r, &buf)
+	txt2srt.Convert(r, &buf, durationOption)
 
 	return buf.String()
 }
